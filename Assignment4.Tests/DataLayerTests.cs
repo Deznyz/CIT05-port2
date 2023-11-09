7b26d0ebcf0f99d062e64d2fdcfe90587635e61c
Rasmus Laue Petersen <rlauep@ruc.dk>
2023-11-09 14:46:58 +0100

    Tests til datalayer

--- Assignment4.Tests/DataLayerTests.cs
+++ Assignment4.Tests/DataLayerTests.cs
@@ -1,4 +1,5 @@
 ﻿using DataLayer;
+using DataLayer.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,13 +11,13 @@ namespace Tests.Tests
     public class DataLayerTests
     {
         [Fact]
-        public void CreateAliases_ValidData_CreteAliasesAndReturnsNewObject()
+        public void Create_and_Delete_Aliases()
         {
             var service = new DataService();
             var alias = service.CreateAliases(
                 new DataLayer.Models.Aliases
                 {
-                    TitleId = "tt0052520 ",
+                    TitleId = "tt12879782",
                     Ordering = 456,
                     Title = "title",
                     Region = "region",
@@ -36,11 +37,59 @@ namespace Tests.Tests
         }
 
         [Fact]
+        public void Create_Update_Aliases()
+        {
+            var service = new DataService();
+            var alias = service.CreateAliases(
+                new DataLayer.Models.Aliases
+                {
+                    TitleId = "tt12879782",
+                    Ordering = 456,
+                    Title = "title",
+                    Region = "region",
+                    Language = "language",
+                    IsOriginalTitle = false,
+                    Types = "types",
+                    Attributes = "attributes"
+                });
+
+
+            Assert.True(alias.Ordering == 456);
+            Assert.Equal("title", alias.Title);
+            Assert.Equal("language", alias.Language);
+
+            alias.Title = "updated";
+
+            service.UpdateAliases(alias.Title, alias.Ordering, alias);
+            Assert.Equal("updated", alias.Title);
+
+            // cleanup
+            service.DeleteAliases(alias);
+        }
+
+        [Fact]
+        public void Get_Alias()
+        {
+            var service = new DataService();
+            var alias = service.GetAlias("tt10687202", 83);
+            Assert.Equal("Ein lustiges Hundeleben", alias.Title);
+        }
+
+        [Fact]
+        public void Get_Aliases()
+        {
+            var service = new DataService();
+            var aliases = service.GetAliases(0,10);
+            Assert.Equal(365359, aliases.count);
+        }
+
+        [Fact]
         public void Get_Co_Actors_test()
         {
             var service = new DataService();
-            service.GetCoActors("Johnny Depp", 0, 10);
-           
+            var coActors = service.GetCoActors("Johnny Depp", 0, 10);
+            Assert.Equal(101, coActors.count);
+
         }
 
     }
--- Assignment4.Tests/WebServerTests.cs
+++ Assignment4.Tests/WebServerTests.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 
 namespace Assignment4.Tests
 {
-    internal class WebServerTests
+    public class WebServerTests
     {
+        
     }
+
 }
+
